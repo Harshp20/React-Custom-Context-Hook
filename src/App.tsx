ebac37826fd254219c027aae26ca4d2d667ae040
Harsh <harsh.pradhan@ext.zestmoney.in>
2022-11-24 14:24:12 +0530

    Re-organised folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css';
+import './styles/App.css'
 import { ConsumerComponent } from './components/ConsumerComponent';
 import { ContextProvider } from './hooks/ContextProvider'
 
